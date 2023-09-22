7b61f5685ccf975ee3ed4a6c28721b619358e201
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-09-22 11:16:29 -0400

    Resume

--- my-app/src/pages/Resume.js
+++ my-app/src/pages/Resume.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Navigation from '../components/containers/Navigation';
+import PageContainer from '../components/containers/PageContainer';
+
 
 function Resume() {
   return (
     <div>
-      <Navigation title='Resume' />
-      <object
-        data="/react-portfolio/my-app/public/JoshuaOBrienRS.pdf"
-        type="application/pdf"
-        width="100%"
-        height="600px"
-      >
-        <p>It seems you don't have a PDF plugin for this browser.
-          you can <a href="/react-portfolio/my-app/public/JoshuaOBrienRS.pdf">click here to download the PDF file.</a>
-        </p>
-      </object>
-      <Navigation title='Resume' />
-      <Resume />
+      <PageContainer title='Resume'>
+        <Navigation title='Resume' />
+        <object
+          data="/my-app/public/JoshuaOBrienRS.pdf"
+          type="application/pdf"
+          width="100%"
+          height="600px"
+        >
+          <p>It seems you don't have a PDF plugin for this browser.
+            You can <a href="/JoshuaOBrienRS.pdf">click here to download the PDF file.</a>
+          </p>
+        </object>
+      </PageContainer>
     </div>
   );
-};
+}
 
 export default Resume;
