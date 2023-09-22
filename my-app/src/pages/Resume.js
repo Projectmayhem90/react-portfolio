67be7d873c248b6abe43065fef0421c5170558b0
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-09-22 11:29:11 -0400

    Fixes to show PDF

--- my-app/src/pages/Resume.js
+++ my-app/src/pages/Resume.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from '../components/containers/Navigation';
 import PageContainer from '../components/containers/PageContainer';
+import ResumeItem from '../components/ResumeModule';
+import classes from './resume.module.css';
 
 
 function Resume() {
@@ -8,7 +10,10 @@ function Resume() {
     <div>
       <PageContainer title='Resume'>
         <Navigation title='Resume' />
-        <object
+        <article className={classes.resumeBox}>
+          <ResumeItem />
+        </article>
+        <Resume
           data="/my-app/public/JoshuaOBrienRS.pdf"
           type="application/pdf"
           width="100%"
@@ -17,7 +22,7 @@ function Resume() {
           <p>It seems you don't have a PDF plugin for this browser.
             You can <a href="/JoshuaOBrienRS.pdf">click here to download the PDF file.</a>
           </p>
-        </object>
+        </Resume>
       </PageContainer>
     </div>
   );
