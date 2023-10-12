ceb5a5c3fa1eebcfa873694d3c464394501ae42b
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-10-12 19:41:43 -0400

    ..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 // Import necessary dependencies
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Work from './pages/Work';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -13,12 +13,14 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 function App() {
   return (
     <BrowserRouter basename='/react-portfolio'>
-    <Routes>
-      {/* <Route path="/about" element={<About />}></Route> */}
-      <Route path="/work" element={<Work />}></Route>
-      <Route path="/contact" element={<Contact />}></Route>
-      <Route path="/" element={<About />} />
-      <Route path="/resume" element={<Resume />}></Route>
+      <Routes>
+        {/* <Route path="/about" element={<About />}></Route> */}
+        <Route path="/work" element={<Work />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/" element={<About />} />
+        <Route path="/resume" element={<Resume />}></Route>
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />     
     </BrowserRouter>
