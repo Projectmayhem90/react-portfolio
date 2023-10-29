d5f7c7129efb182e4424bd8ae806b7c6f8e123a4
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-10-29 15:08:04 -0400

    Added styling to add header image backin, looked to plain without. Updated Media Q's

--- my-app/src/components/containers/Header.js
+++ my-app/src/components/containers/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import banner from '../../images/Header-Do-SMTHG-GRT.jpg';
 import Navigation from './Navigation';
 import styles from './Header.module.css';
 
 function Header(props) {
-  
+  const headerStyle = {
+    backgroundImage: `url(${banner})`,
+  };
+
   return (
     <header className={styles.headerContainer}>
-      <div className={`${styles.header} ${styles.dynamicBackground}`}>
+      <div className={styles.header} style={headerStyle}>
          {/* Other header content if needed */}
       </div>
       <Navigation />
