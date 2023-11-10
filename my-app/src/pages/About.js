015250b7b3089c35d4709983585b3ae671a6d226
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-11-10 13:42:19 -0500

    ..

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -15,7 +15,7 @@ function About(props) {
           src={profile}
           alt="" />
         <div className={classes.aboutContent}>
-            Throughout my professional journey in my twenties, I underwent a range of occupations without feeling a sense of passion towards any particular field. However, in 2022, I made a conscious decision to pursue a long-held aspiration of mine - web-based coding.
+            Throughout my professional journey in my twenties, I underwent a range of occupations without feeling a sense of passion towards any particular field. However, in 2022, I made a conscious decision to pursue a long-held aspiration of mine - software development.
           My work history boasts diverse experiences such as serving as a Construction Project Deployment Manager, with responsibilities that encompassed supervising significant multi-million dollar remodels. I also held the position of Geek Squad Manager, overseeing daily interactions in PC repair and Car installation. In both roles, I honed my skills in employee development and training.
           <br />
           <br />
